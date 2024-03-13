5dc9a72586a87547bb9ed72a21c0e1e908299d43
Charles Vanderhoff <charles@vandersoft.co>
2024-03-13 12:42:48 -0700

    initial commit

---
+++ App/App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Charles Vanderhoff on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
